965c5b42942944f65bfccd3b8d913beb5c2c2744
Harshal141 <harshalmukundapatil@gmail.com>
2023-04-13 16:05:28 +0530

    final ui design

--- assets/app.js
+++ assets/app.js
@@ -4,9 +4,7 @@ let panel_content = document.querySelector('.panel_content_left');
 
 // onscroll event
 window.onscroll = function() {
-    // translate body
-    // constrain.style.marginTop = `-${window.scrollY}px`;
-    // translate panel_left
+    
     panel_left.style.marginTop = `${window.scrollY/10}px`;
     // translate panel_content down
     panel_content.style.marginTop = `-${window.scrollY}px`;
